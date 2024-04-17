c034e61f091ff1f991719620af4db1a5e173c75f
elokrupek <105728569+elokrupek@users.noreply.github.com>
2024-04-17 13:39:09 -0300

    Create funcoes_javascript.js

---
+++ funcoes_javascript.js
@@ -0,0 +1,12 @@
+fuction somar(num1, num2{
+  return num1+num2;
+}
+fuction diminuir(num1, num2{
+  return num1-num2;
+}
+fuction mutiplicar(num1, num2{
+  return num1*num2;
+}
+fuction dividir(num1, num20{
+  return num1/num2;
+}
